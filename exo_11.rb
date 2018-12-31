0e8a540a1636c846cd87a3359854299821d6294d
NIcoZF <nico-7@hotmail.fr>
2018-12-31 12:05:48 +0100

    ajout exo 13

---
+++ exo_11.rb
@@ -0,0 +1,6 @@
+puts "Donne moi un nombre"
+user_number = gets.chomp
+
+user_number.to_i.times do |i|
+    puts "Salut, ça farte ?"
+end
---
+++ exo_12.rb
@@ -0,0 +1,9 @@
+puts  "Donne moi un nombre"
+user_number = gets.chomp
+
+i= 0
+
+user_number.to_i.times do |i|
+  puts "#{i}"
+
+end
