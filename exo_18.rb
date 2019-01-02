ac638b4df95f33ee06b117479faa20672752f0c8
NIcoZF <nico-7@hotmail.fr>
2019-01-02 11:53:23 +0100

    exo 18 ok

--- exo_18.rb
+++ exo_18.rb
@@ -1,13 +1,18 @@
+emails_array = []
+
+
 i= 1
+
+
 
 while i < 51
 
   if i > 0 && i < 10
-  puts "jean.dupont0#{i}@emails.fr"
+  emails_array.push (puts "jean.dupont0#{i}@emails.fr")
 
 else
 
-  puts "jean.dupont#{i}@emails.fr"
+  emails_array.push (puts "jean.dupont#{i}@emails.fr")
 
 end
 
