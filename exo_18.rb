03d5484882acef380033f8c87e2205c2ef6aa0f6
NIcoZF <nico-7@hotmail.fr>
2019-01-02 12:27:01 +0100

    exo 18 maj

--- exo_18.rb
+++ exo_18.rb
@@ -8,13 +8,15 @@ i= 1
 while i < 51
 
   if i > 0 && i < 10
-  emails_array.push (puts "jean.dupont0#{i}@emails.fr")
+  emails_array.push("jean.dupont0#{i}@emails.fr")
 
 else
 
-  emails_array.push (puts "jean.dupont#{i}@emails.fr")
+  emails_array.push("jean.dupont#{i}@emails.fr")
 
 end
 
   i = i + 1
 end
+
+print emails_array
